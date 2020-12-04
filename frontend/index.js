a0672ef4f9f1229a08e00b1c3ae6d8e0b2c7a775
rpsh88 <rpsh88@durham.ac.uk>
2020-12-04 21:08:53 +0800

    Fixed carousel buttons to be responsive

--- frontend/index.js
+++ frontend/index.js
@@ -22,44 +22,44 @@ inputForm.addEventListener('submit', function (event) {
     window.alert(error);
   }
 });
-var x = 0
+
+var x = false
 const play = "<svg width=\"2em\" height=\"2em\" viewBox=\"0 0 16 16\" class=\"bi bi-play\" fill=\"currentColor\"><path fill-rule=\"evenodd\" d=\"M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z\"/></svg>"
 const pause = "<svg width=\"2em\" height=\"2em\" viewBox=\"0 0 16 16\" class=\"bi bi-pause\" fill=\"currentColor\"><path fill-rule=\"evenodd\" d=\"M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z\"/></svg>"
 const playPausebtn = document.getElementById('playPause');
+
 playPausebtn.addEventListener('click', function () {
-  if (x == 0){
+  if (!x){
     playPausebtn.innerHTML = pause
-    x = 1
+    x = !x
+    playPausebtn.disabled = true
+    setTimeout(function(){
+      playPausebtn.disabled = false;
+    },1000);
   }
   else{
     playPausebtn.innerHTML = play
-    x = 0
+    x = !x
   }
 });
 
-
 $(document).ready(function () {
-  console.log("Pause");
   $('#playPause').click(function () {
     if ($("#myCarousel").attr("data-interval")) {
-
       $("#myCarousel").removeAttr("data-interval");
       $(".carousel-item>.active").removeAttr("active");
-      setTimeout(function () {
-        $(".carousel-control-next").trigger('click');
-        $(".carousel-inner").trigger('mouseenter');
-        $("#myCarousel").trigger('mouseenter');
-      });
-    } else {
-      console.log("Play");
-
-      $("#myCarousel").attr("data-interval", "1500");
-      setTimeout(function () {
+      $(".carousel-control-next").trigger('click');
+      $(".carousel-inner").trigger('mouseenter');
+      $("#myCarousel").trigger('mouseenter');
+    } else { 
+      setTimeout(function(){
+        console.log("Play");
+        $("#myCarousel").attr("data-interval", "1000");
         $(".carousel-control-next").trigger('click');
         $(".carousel-inner").trigger('mouseenter');
         $("#myCarousel").trigger('mouseenter');
         $("#myCarousel").trigger('mouseleave');
-      });
+      },1000);
     }
   });
 
