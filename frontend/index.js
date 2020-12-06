9af1e35acbb733be36d8bbf167d313f86aefb2ec
rpsh88 <rpsh88@durham.ac.uk>
2020-12-06 20:12:04 +0800

    redone slideshow

--- frontend/index.js
+++ frontend/index.js
@@ -26,48 +26,71 @@ inputForm.addEventListener('submit', function (event) {
 var x = false
 const play = "<svg width=\"2em\" height=\"2em\" viewBox=\"0 0 16 16\" class=\"bi bi-play\" fill=\"currentColor\"><path fill-rule=\"evenodd\" d=\"M10.804 8L5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z\"/></svg>"
 const pause = "<svg width=\"2em\" height=\"2em\" viewBox=\"0 0 16 16\" class=\"bi bi-pause\" fill=\"currentColor\"><path fill-rule=\"evenodd\" d=\"M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z\"/></svg>"
-const playPausebtn = document.getElementById('playPause');
+const spinner = "<div class=\"spinner-border\" role=\"status\"><span class=\"sr-only\">Loading...</span></div>"
+const startCycle = document.getElementById('startCycle');
 
-playPausebtn.addEventListener('click', function () {
+startCycle.addEventListener('click', function () {
   if (!x){
-    playPausebtn.innerHTML = pause
+    startCycle.innerHTML = spinner
     x = !x
-    playPausebtn.disabled = true
+    startCycle.disabled = true
     setTimeout(function(){
-      playPausebtn.disabled = false;
+      startCycle.disabled = false;
+      startCycle.innerHTML = pause;
     },1000);
   }
   else{
-    playPausebtn.innerHTML = play
+    startCycle.innerHTML = play
     x = !x
   }
 });
 
-$(document).ready(function () {
-  $('#playPause').click(function () {
-    if ($("#myCarousel").attr("data-interval")) {
-      $("#myCarousel").removeAttr("data-interval");
-      $(".carousel-item>.active").removeAttr("active");
-      $(".carousel-control-next").trigger('click');
-      $(".carousel-inner").trigger('mouseenter');
-      $("#myCarousel").trigger('mouseenter');
-    } else { 
-      setTimeout(function(){
-        console.log("Play");
-        $("#myCarousel").attr("data-interval", "1000");
-        $(".carousel-control-next").trigger('click');
-        $(".carousel-inner").trigger('mouseenter');
-        $("#myCarousel").trigger('mouseenter');
-        $("#myCarousel").trigger('mouseleave');
-      },1000);
-    }
-  });
+var imgNumber = 0;
+var path = ["Figure_1.png","Figure_2.png",
+  "Figure_3.png",
+  "Figure_4.png"
+];
+var numberOfImg = path.length;
+var timer = null;
 
-  // Enable Carousel Controls
-  $(".carousel-control-prev").click(function () {
-    $("#myCarousel").carousel("prev");
-  });
-  $(".carousel-control-next").click(function () {
-    $("#myCarousel").carousel("next");
-  });
-});
+function slide() {
+  imgNumber = (imgNumber + 1) % path.length;
+  console.log(imgNumber);
+  document.getElementById("imgSlideshow").src = path[imgNumber];
+  changeCounter(imgNumber + 1, numberOfImg);
+}
+
+function setTimer() {
+  if (timer) {
+    clearInterval(timer);
+    timer = null;
+  } else {
+    timer = setInterval(slide, 2000);
+  }
+  return false;
+}
+
+function previousImage() {
+  --imgNumber;
+  if (imgNumber < 0) {
+    imgNumber = numberOfImg - 1;
+  }
+  document.getElementById("imgSlideshow").src = path[imgNumber];
+  changeCounter(imgNumber + 1, numberOfImg);
+  return false;
+}
+
+function nextImage() {
+  ++imgNumber;
+  if (imgNumber > (numberOfImg - 1)) {
+    imgNumber = 0;
+  }
+  document.getElementById("imgSlideshow").src = path[imgNumber];
+  changeCounter(imgNumber + 1, numberOfImg);
+  return false;
+}
+
+function changeCounter(cur, total) {
+  document.getElementById("counter").innerHTML = cur + "/" + total;
+}
+document.getElementById("counter").innerHTML = 1 + "/" + path.length;
